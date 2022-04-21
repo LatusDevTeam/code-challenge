842875f6dda9eadb3ac56b37bae30e9c793e664e
Ross Cooper <ross@craftaweb.co.uk>
2022-04-21 23:57:17 +0100

    UI tweaks

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -6,39 +6,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script src="https://cdn.tailwindcss.com"></script>
 
-    <title>Code Challenge</title>
-    <style>
-        html,
-        body {
-            background-color: #fff;
-            color: #636b6f;
-            font-family: sans-serif;
-            height: 100vh;
-            margin: 50px;
-        }
-
-        .full-height {
-            height: 100vh;
-        }
-
-        .result {}
-    </style>
+    <title>Search Results</title>
 </head>
 
 <body>
-    <div class="full-height">
-        <div class="result">
+    <div class="min-h-screen bg-slate-900 text-white">
+        <div class="p-3 text-white">
             Your Search Term Was: <b>{{$searchTerm}}</b>
         </div>
         <div class="grid grid-cols-3">
             <div class="m-1 rounded bg-white shadow p-3 bg-slate-800 text-white">
-                <h2 class="font-bold text-xl text-green-400">Artists</h2>
+                <h2 class="font-bold text-xl text-green-500 mb-3">Artists</h2>
                 @if(count($artists))
                 @foreach($artists as $artist)
                 @include('parts.result', [
                 'id' => $artist->id,
                 'name' => $artist->name,
                 'image' => $artist->images[0]->url ?? null,
+                'type' => 'artists',
                 ])
                 @endforeach
                 @else
@@ -46,13 +31,14 @@
                 @endif
             </div>
             <div class="m-1 rounded bg-white shadow p-3 bg-slate-800">
-                <h2 class="font-bold text-xl text-green-400">Tracks</h2>
+                <h2 class="font-bold text-xl text-green-500 mb-3">Tracks</h2>
                 @if(count($tracks))
                 @foreach($tracks as $track)
                 @include('parts.result', [
                 'id' => $track->id,
                 'name' => $track->name,
                 'image' => $track->album->images[0]->url ?? null,
+                'type' => 'tracks',
                 ])
                 @endforeach
                 @else
@@ -60,13 +46,14 @@
                 @endif
             </div>
             <div class="m-1 rounded bg-white shadow p-3 bg-slate-800">
-                <h2 class="font-bold text-xl text-green-400">Albums</h2>
+                <h2 class="font-bold text-xl text-green-500 mb-3">Albums</h2>
                 @if(count($albums))
                 @foreach($albums as $album)
                 @include('parts.result', [
                 'id' => $album->id,
                 'name' => $album->name,
                 'image' => $album->images[0]->url ?? null,
+                'type' => 'albums',
                 ])
                 @endforeach
                 @else

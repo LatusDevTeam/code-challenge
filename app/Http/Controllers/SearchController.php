6a31bb73b596917a36199c24702508211ecacf6e
Ross Cooper <ross@craftaweb.co.uk>
2022-04-21 23:17:56 +0100

    Added search function to controller

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Interfaces\SpotifyApiInterface;
 use Illuminate\Http\Request;
 
 class SearchController extends Controller
@@ -12,10 +13,17 @@ class SearchController extends Controller
     }
 
 
-    public function search(Request $request)
+    public function search(Request $request, SpotifyApiInterface $spotify)
     {
         $query = $request->get('query');
+        $search = $spotify->search($query, ['artist', 'album', 'track',]);
 
-        return view('search', ['searchTerm' => $query]);
+
+        return view('search', [
+            'searchTerm' => $query,
+            'artists' => $search->artists->items,
+            'tracks' => $search->tracks->items,
+            'albums' => $search->albums->items,
+        ]);
     }
 }
